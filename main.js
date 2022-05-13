7e94d274d371c0d7bdff2aac13bb657e748995c5
SamuelBurnes <srburnes02@gmail.com>
2022-05-13 08:28:27 -0500

    Test 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const tiles = document.querySelectorAll("div[class~='_7Ge4LWt06zuiD6QZsE91+Q==']");
-Array.from(tiles).forEach(tile => {
+(Array.from(tiles)).forEach(tile => {
 	console.log(tile.firstChild.firstChild.children[1].textContent);
 });
