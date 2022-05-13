3cfcd01af00d9c81a550de388940d66affeb3e27
SamuelBurnes <srburnes02@gmail.com>
2022-05-13 08:30:18 -0500

    Test 5

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const tiles = document.querySelectorAll("div[class~='_7Ge4LWt06zuiD6QZsE91+Q==']");
 console.log("For Loop Start");
 console.log(tiles);
-(Array.from(tiles)).forEach(tile => {
-	console.log(tile.firstChild.firstChild.children[1].textContent);
-});
+//(Array.from(tiles)).forEach(tile => {
+//	console.log(tile.firstChild.firstChild.children[1].textContent);
+//});
