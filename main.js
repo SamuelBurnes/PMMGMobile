ad65b1c8e89ec4b7e9d03ec0a6af330460f53755
SamuelBurnes <srburnes02@gmail.com>
2022-05-13 08:25:45 -0500

    Test Tile Selection

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-document.querySelectorAll("div[class~='_7Ge4LWt06zuiD6QZsE91+Q==']");
-console.log("Here!");
+const tiles = document.querySelectorAll("div[class~='_7Ge4LWt06zuiD6QZsE91+Q==']");
+Array.from(tiles).forEach(tile -> {
+	console.log(tile.firstChild.firstChild.children[1].textContent);
+});
