e46cfd9639a35f9a67851dd022b231211c6271d8
SamuelBurnes <srburnes02@gmail.com>
2022-05-19 16:21:27 -0500

    NOTS Test 2

--- main.js
+++ main.js
@@ -6,15 +6,32 @@ class PMMGMobile {
 		window.setTimeout(() => this.loop(), 1000);
 	}
 	
+	cleanup(className)
+	{
+		Array.from(document.getElementsByClassName(className)).forEach(elem => {
+			elem.parentNode.removeChild(elem);
+		});
+	}
+
 	nots_recolor()
 	{
 		try
 		{
+			this.cleanup("pmmg-nots");
 			const container = document.getElementById("container");
 			const buffer = container.firstChild.firstChild.children[1].children[1].firstChild.firstChild;
 			if(buffer.firstChild.firstChild.textContent === "Buffer / NOTS")
 			{
-				console.log("NOTS Detected");
+				const notsElem = buffer.children[1].firstChild;
+				Array.from(notsElem).forEach(nots => {
+					if(nots.classList.includes("_6iTMJZ+xm-PbG+nWoPqh7g=="))
+					{
+						const notType = document.createElement("div");
+						notType.classList.add("pmmg-nots");
+						notType.textContent = "test";
+						nots.children[1].insertBefore(notType, nots.children[1].children[0]);
+					}
+				});
 			}
 		} catch(e)
 		{console.log("NOTS Not Detected")}
