52562cf12e77332f770a1136fa0ca973905f7d0b
SamuelBurnes <srburnes02@gmail.com>
2022-05-19 15:42:30 -0500

    Test 10

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 console.log("Start");
-setTimeout( function(){
-const container = document.getElementById("container");
-console.log(container);
-}, 5000);
-const tiles = document.querySelector("div.E4k-KlbccYxDX+4RCvyOIA==");
-console.log("For Loop Start");
-console.log(tiles);
-Array.from(tiles).forEach(tile => {
-	console.log(tile.firstChild.firstChild.children[1].textContent);
-});
+
+const runner = new PMMGMobile();
+runner.loop();
+
+class PMMGMobile {
+	loop(){
+		console.log("Looped");
+
+		window.setTimeout(() => this.loop(), 1000);
+	}
+
+}
