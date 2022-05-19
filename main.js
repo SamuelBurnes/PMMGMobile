03a7e8880861a7d43fad9b0cc680bfb66a66a4d4
SamuelBurnes <srburnes02@gmail.com>
2022-05-19 16:22:58 -0500

    NOTS Test 3

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ class PMMGMobile {
 	{
 		try
 		{
+			console.log("Start Cleanup");
 			this.cleanup("pmmg-nots");
+			console.log("End Cleanup");
 			const container = document.getElementById("container");
 			const buffer = container.firstChild.firstChild.children[1].children[1].firstChild.firstChild;
 			if(buffer.firstChild.firstChild.textContent === "Buffer / NOTS")
@@ -26,6 +28,7 @@ class PMMGMobile {
 				Array.from(notsElem).forEach(nots => {
 					if(nots.classList.includes("_6iTMJZ+xm-PbG+nWoPqh7g=="))
 					{
+						console.log("Processing Not");
 						const notType = document.createElement("div");
 						notType.classList.add("pmmg-nots");
 						notType.textContent = "test";
