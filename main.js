2fc72ad8e886c6e015867d54373df5cf8c92abdc
SamuelBurnes <srburnes02@gmail.com>
2022-05-19 15:33:29 -0500

    Test 9

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+console.log("Start");
+setTimeout( function(){
+const container = document.getElementById("container");
+console.log(container);
+}, 5000);
 const tiles = document.querySelector("div.E4k-KlbccYxDX+4RCvyOIA==");
 console.log("For Loop Start");
 console.log(tiles);
